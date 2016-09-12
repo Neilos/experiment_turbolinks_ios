3e42ed9f0c4f861df179571225cdbe90394a9a00
Neil Atkinson <neil@bluespot.io>
2016-09-12 13:56:39 +0100

    Initial Commit

---
+++ MyApplicationIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyApplicationIOS
+//
+//  Created by N Atkinson on 12/09/2016.
+//  Copyright © 2016 N Atkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
